acbb79f80b524c8de8ae87340b4f816ce1f84a01
TessiMarks <tessimariamarks@gmail.com>
2021-04-12 16:27:03 +0200

    hier er bij

---
+++ Opdracht_1.java
@@ -0,0 +1,10 @@
+//import java.util.Scanner;
+class Demo {
+    public static void main(String[] args) {
+        String str1 = "ko";
+        String str2 = "nijn";
+        String str3 = "tjes";
+        String result = str1 + str2 + str3;
+        System.out.println(result);
+    }   
+}
